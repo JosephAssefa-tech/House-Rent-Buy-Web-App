899f7565cbf3ceff29058e80ca17faeeb0626ce7
Joseph <yosephassefa85@gmail.com>
2021-12-18 01:13:47 +0300

    newly added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NgModule } from '@angular/core';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    NavBarComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
