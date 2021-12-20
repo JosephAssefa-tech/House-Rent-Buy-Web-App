9f300142705fc752365d3df8ee40651ca60f7f57
Joseph <yosephassefa85@gmail.com>
2021-12-20 20:45:08 +0300

    worked on pagination

--- src/app/property/property-detail/property-detail/property-detail.component.ts
+++ src/app/property/property-detail/property-detail/property-detail.component.ts
@@ -1,6 +1,5 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-property-detail',
@@ -10,10 +9,24 @@ import { ActivatedRoute } from '@angular/router';
 export class PropertyDetailComponent implements OnInit {
   public propertyId:number={} as number;
 
-  constructor(private activatedRoute:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute,private router:Router) { }
 
   ngOnInit(): void {
-    this.propertyId=this.activatedRoute.snapshot.params['id'];
+    this.propertyId=+this.route.snapshot.params['id'];
+
+    this.route.params.subscribe(
+      (params)=>{
+        this.propertyId= +params['id'];
+      }
+    );
+
+  }
+  onSelectNext()
+  {
+    this.propertyId+=1;
+    this.router.navigate(['property-detail', this.propertyId]);
+
+
 
   }
 
